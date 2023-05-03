24567d030045544da5d102b48f924a6756338f80
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-05-04 03:20:47 +0700

    :art: add User model :art:

---
+++ src/main/java/KunKruKrab/schedule/model/User.java
@@ -0,0 +1,20 @@
+package KunKruKrab.schedule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String email;
+    private String password;
+    private String name;
+    private String role;
+}
