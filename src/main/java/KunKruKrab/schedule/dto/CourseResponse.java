680fdc0458dc3ed8f8edd87c27fd8ec290a3830d
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-05-11 18:49:34 +0700

    feat: Add course response dto

--- src/main/java/KunKruKrab/schedule/dto/CourseResponse.java
+++ src/main/java/KunKruKrab/schedule/dto/CourseResponse.java
@@ -1,2 +1,12 @@
-package KunKruKrab.schedule.dto;public class CourseResponse {
+package KunKruKrab.schedule.dto;
+
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class CourseResponse {
+    private UUID id;
+    private String classCode;
+    private String name;
+    private String description;
 }
