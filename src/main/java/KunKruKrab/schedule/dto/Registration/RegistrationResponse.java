658e15249ea61ea1dac157b778345cdc56566e3f
Bhokin Watanapitak <zbeamz@hotmail.com>
2023-05-12 13:59:59 +0700

    Fix registration request error

--- src/main/java/KunKruKrab/schedule/dto/Registration/RegistrationResponse.java
+++ src/main/java/KunKruKrab/schedule/dto/Registration/RegistrationResponse.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 @Data
 public class RegistrationResponse {
-    private String courseID;
+    private UUID courseID;
     private UUID userID;
     private Instant createdAt;
 }
